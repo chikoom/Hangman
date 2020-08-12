9eecf6b68653ace40368555e12e0c729a0c5ee44
Chikoom <callforcopy@gmail.com>
2020-08-12 08:12:19 +0300

    done

--- src/components/Solution.jsx
+++ src/components/Solution.jsx
@@ -1,13 +1,15 @@
-import React, { Component } from 'react' 
+import React, { Component } from 'react'
 import Letter from './Letter'
 class Solution extends Component {
-  render(){
-    const word = ['_','_','_','_']
-    const hint = 'Guess what!'
-    return(
+  render() {
+    return (
       <div>
-          <h3>{this.props.solution.word.split('').map(letter=><Letter letter={this.props.letterStatus[letter]?letter:'_'} />)}</h3>
-          <h4>Hint: {this.props.solution.hint}</h4>
+        <h3>
+          {this.props.solution.word.split('').map(letter => (
+            <Letter letter={this.props.letterStatus[letter] ? letter : '_'} />
+          ))}
+        </h3>
+        <h4>Hint: {this.props.solution.hint}</h4>
       </div>
     )
   }
